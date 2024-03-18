a4456d3148dc0a88db4262bfc5ad835b20629fb3
migueldiasbrito <migueldiasbrito@gmail.com>
2024-03-18 16:03:27 +0000

    WIP Connections

---
+++ unity-client/Assets/Scripts/Dots/Presentation/ConnectionUiDisplay.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Mdb.Ctd.Dots.Presentation
+{
+    public class ConnectionUiDisplay : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+    }
+}
